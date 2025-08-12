1f51dd317cca5e8bf74ffc497add199b0210fff0
tomi-912 <to912mmy.gm@gmail.com>
2025-08-12 10:59:10 +0900

    test commit

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ fn main() {
             DefaultPlugins
                 .set(WindowPlugin {
                     primary_window: Some(Window {
-                        title: "Bevy game".to_string(), // ToDo
+                        title: "my bevy game".to_string(), // ToDo
                         // Bind to canvas included in `index.html`
                         canvas: Some("#bevy".to_owned()),
                         fit_canvas_to_parent: true,
